fc42c288c0d255f47053835799550a64c324d997
Maria Kunevich <maria.kunevich@gmail.com>
2021-12-20 19:28:11 +0200

    upload

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import './App.css';
 
-class App extends Component {
-    render() {
-        return (
-           <>   <h1>My Habit Tracker</h1>
-           </>
-        )
-    }
-}
+function App(props) {
+    return (
+      <div className="app stack">
+        <h1>My Habit Tracker</h1>
+        <div className="habit-container"></div> 
+        <form>
+        <div className="add-habit">
+          <input className="habit" type="text" 
+          name="habit" placeholder="Enter Habit Name" required />
+           <input type="number" name="reps" 
+        placeholder="# of Repetitions" min={1} required />
+        </div>
+         <div className="frequency">
+        <label htmlFor="timeframe">Frequency: </label>
+        <select name="timeframe" id="timeframe">
+          <option value="Daily">Daily</option>
+          <option value="Weekly">Weekly</option>
+          <option value="Monthly">Monthly</option>
+          <option value="Yearly">Yearly</option> 
+          </select>  
+        </div>
+        <input type="submit" value="Add Habit" />
+        </form>
+        <ul className="habits">
+        <li>Loading...</li>
+      </ul>
+    </div>
+    );
+  }
+
 export default App;
