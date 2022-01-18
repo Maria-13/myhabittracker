f53590a85921809fb74f2c5eb9eab9bfa8542e29
Maria Kunevich <maria.kunevich@gmail.com>
2022-01-18 15:48:14 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-iimport React from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App tasks={DATA} />
   </React.StrictMode>,
   document.getElementById('root')
 );
